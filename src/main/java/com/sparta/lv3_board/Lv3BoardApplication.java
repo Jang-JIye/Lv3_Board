21a9af3e99bc4e7d8a26df2c1df7eeb1d2219ce2
Jang-JIye <jjy2646@gmail.com>
2023-09-06 23:27:37 +0900

    init commit

---
+++ src/main/java/com/sparta/lv3_board/Lv3BoardApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.lv3_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lv3BoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lv3BoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/lv3_board/Lv3BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.lv3_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lv3BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
